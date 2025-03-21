0cb7f450da03e5285705fa7dd3ba088dccd1a0da
sabilar-rahman <sabilarrahman36@gmail.com>
2025-03-22 03:12:14 +0600

    added: shift CRUD Operation Successfully

--- src/routes/shiftRoutes/shiftRoutes.ts
+++ src/routes/shiftRoutes/shiftRoutes.ts
@@ -1,10 +1,20 @@
-import { createClassController } from "@/controllers/classController/classController";
-import { createShiftController } from "@/controllers/shiftController/shiftController";
+import { createShiftController, deleteShiftController, getAllShiftController, getShiftControllerById, updateShiftController } from "@/controllers/shiftController/shiftController";
 import { Router } from "express";
+
+
+
+
 
 
 const shiftRouts = Router();
 
 shiftRouts.post("/", createShiftController);
+shiftRouts.get("/", getAllShiftController);
+shiftRouts.get("/:id", getShiftControllerById);
+shiftRouts.patch("/:id", updateShiftController);
+shiftRouts.delete("/:id", deleteShiftController);
+
+
+
 
 export default shiftRouts;
