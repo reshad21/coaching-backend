60d60c007ad50087f31c1e2f585c4d5b424099ce
reshad21 <miakala7@gmail.com>
2026-05-07 14:22:52 +0600

    new file added

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/server.ts'], 
+  entry: ['src/server.ts'],
   outDir: 'build',
   target: 'es2020',
-  format: ['esm'],
+  format: ['cjs'], // Use CommonJS for Vercel Node
   sourcemap: true,
   clean: true,
   dts: false,
